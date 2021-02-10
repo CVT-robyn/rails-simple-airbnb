dbe256d70bfe8dcde2c8656f4170140e47bc7a90
ROBYN PERRIN <robynperrin76@gmail.com>
2021-02-10 15:13:55 +0100

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating database..."
+10.times do
+  flat = Flat.new(
+    name: Faker::Lorem.sentence,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(20..150),
+    number_of_guests: rand(1..15)
+  )
+
+  flat.save
+  puts flat.name
+end
+puts 'Seed Done'
